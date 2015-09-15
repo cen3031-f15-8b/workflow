557a30785f1fa326d5b3529f660031a0be6cf87b
Khaled Hassan <khaled.hassan@gmail.com>
2015-09-14 20:56:16 -0400

    add task priorities

--- todo-src/script.js
+++ todo-src/script.js
@@ -3,34 +3,43 @@
 var myApp = angular.module('app', []);
 
 myApp.controller('MainCtrl', function ($scope){
+  var priority = {
+    NOW: 0,
+    TOMORROW: 1,
+    SOMEDAY: 2
+  };
 
   $scope.todos = [
     {
-      text: "Learn Angular"
+      text: "Learn Angular",
+      priority: priority.NOW
     },
     {
-      text: "Learn node"
+      text: "Learn node",
+      priority: priority.NOW
     }
   ];
+  $scope.priorities = priority;
 
   $scope.newItem = "";
+  $scope.newItemPriority = priority.NOW;
 
   $scope.addItem = function(){
     console.log("in add");
     if ($scope.newItem !== ""){
       $scope.todos.push({
         text: $scope.newItem,
+        priority: $scope.newItemPriority
       });
       $scope.newItem = "";
+      $scope.newItemPriority = priority.NOW;
     }
   }
-    
+
   $scope.deleteItem = function(index){
     console.log("in delete");
     $scope.todos.splice(index, 1);
   }
-    
-  
 });
 
 /*************************
